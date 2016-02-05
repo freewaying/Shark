29f45c0e79956f189dc5ca4fc2bb45b9ae3e5272
Kaan Dedeoglu <kaandedeoglu@me.com>
2016-02-05 16:29:34 +0200

    Update to latest swift snapshot February 3, 2016

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ let package = Package(
 
   dependencies: [
     .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 6),
-    .Package(url: "https://github.com/apple/swift-package-manager.git", majorVersion: 0),
     .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 1)
   ]
 )
